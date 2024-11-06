5c969c7e910be2ef3f5b89eff1c444826db0b688
choseongchan3712 <choseongchan3712@naver.com>
2024-11-07 06:40:36 +0900

    style 1st setting

--- src/GlobalStyled.tsx
+++ src/GlobalStyled.tsx
@@ -1,6 +1,62 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+interface PaddingStyle {
+  pcPadding: string;
+  buttonPadding: string;
+  subButtonPadding: string;
+};
+
+interface ColorStyle {
+  mainColor: string;
+  pointColor: string;
+  subFontColor: string;
+  buttonBasicColor: string;
+  buttonHoverColor: string;
+  subButtonColor: string;
+  bgColor:string;
+}
+
+interface FontSize {
+  pcLarge: string;
+  pcHeadline: string;
+  pcTitle: string;
+  pcLabel: string;
+}
+
+interface FontWeight {
+  pcbold: string;
+  pcBasic: string;
+}
+
+export const paddingStyle: PaddingStyle = {
+  pcPadding: "40px",
+  buttonPadding: "10px 15px",
+  subButtonPadding: "2px 5px",
+};
+
+export const colorStyle: ColorStyle = {
+  mainColor: "#fff",
+  pointColor: "salmon",
+  subFontColor: "#afafaf",
+  buttonBasicColor: "#1c2024",
+  buttonHoverColor: "#33363a",
+  subButtonColor: "#1a1a1a",
+  bgColor: "#000",
+};
+
+export const fontSize: FontSize = {
+  pcLarge: "62px",
+  pcHeadline: "30px",
+  pcTitle: "18px",
+  pcLabel: "15px",
+};
+
+export const fontweight: FontWeight = {
+  pcbold: "900",
+  pcBasic: "700",
+};
+
 export const GlobalStyled = createGlobalStyle`
 ${reset}
 
